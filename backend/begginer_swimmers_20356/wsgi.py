6477327f0ad3e63fca98df1bf6add1f3c5186de8
Crowdbotics <team@crowdbotics.com>
2020-09-17 02:28:28 +0000

    Update models

--- backend/begginer_swimmers_20356/wsgi.py
+++ backend/begginer_swimmers_20356/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'begginer_swimmers_20356.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "begginer_swimmers_20356.settings")
 
 application = get_wsgi_application()
